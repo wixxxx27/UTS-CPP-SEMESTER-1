8cf4bf53120f784e76609560f8e2296a86ba3f1f
Dwiyandra Raysha Putra Syawal <dwiyandraraysha@gmail.com>
2024-10-02 16:31:31 +0700

    nomer_5.cpp

--- nomer_5.cpp
+++ nomer_5.cpp
@@ -23,16 +23,16 @@ int main()
 
      
     cout << "Kategori berat badan: ";
-    if (IMT_integer < 18,4) {
+    if (IMT_integer < 18.4) {
         cout << "Berat badan kurang  " << endl;
 
-    } else if (IMT_integer >= 18,5 && IMT_integer <= 24,9) {
+    } else if (IMT_integer >= 18.5 && IMT_integer <= 24.9) {
         cout << "Berat badan ideal" << endl;
 
-    } else if (IMT_integer > 25 && IMT_integer <= 29,9) {
+    } else if (IMT_integer > 25 && IMT_integer <= 29.9) {
         cout << "Berat badan berlebih" << endl;
     }
-        else if (IMT_integer > 30 && IMT_integer <= 39,9) {
+        else if (IMT_integer > 30 && IMT_integer <= 39.9) {
         cout << "Gemuk" << endl;
 
     } else {
